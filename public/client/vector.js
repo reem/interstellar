9150fbbf2d498af4093a80f4793875bc71e9489f
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-19 11:58:56 -0700

    Moved vector type out of legacy status.

--- public/client/vector.js
+++ public/client/vector.js
@@ -1,34 +1,40 @@
-var Vector = function VectorConstructor(x, y) {
-  this.x = x || 0;
-  this.y = y || 0;
-};
+var Vector = {};
 
-Vector.fromAngle = function VectorFromAngle (magnitude, angle) {
-  return new Vector(magnitude * Math.cos(angle), magnitude * Math.sin(angle));
-};
+(function (exports) {
+  var Vector = function VectorConstructor(x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
+  };
 
-Vector.prototype.add = function VectorAdd(other) {
-  this.x += other.x;
-  this.y += other.y;
-};
+  Vector.fromAngle = function VectorFromAngle (magnitude, angle) {
+    return new Vector(magnitude * Math.cos(angle), magnitude * Math.sin(angle));
+  };
 
-Vector.prototype.magnitude = function VectorMagnitude() {
-  return Math.sqrt(this.x * this.x + this.y * this.y);
-};
+  Vector.prototype.add = function VectorAdd(other) {
+    this.x += other.x;
+    this.y += other.y;
+  };
 
-Vector.prototype.angle = function VectorAngle() {
-  return Math.atan2(this.y, this.x);
-};
+  Vector.prototype.magnitude = function VectorMagnitude() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  };
 
-Vector.prototype.scale = function VectorScale(scalar) {
-  this.x *= scalar;
-  this.y *= scalar;
-};
+  Vector.prototype.angle = function VectorAngle() {
+    return Math.atan2(this.y, this.x);
+  };
 
-Vector.prototype.immutableScale = function VectorImmutableScale (scalar) {
-  return new Vector(this.x * scalar, this.y * scalar);
-};
+  Vector.prototype.scale = function VectorScale(scalar) {
+    this.x *= scalar;
+    this.y *= scalar;
+  };
 
-Vector.prototype.clone = function VectorClone() {
-  return new Vector(this.x, this.y);
-};
+  Vector.prototype.immutableScale = function VectorImmutableScale (scalar) {
+    return new Vector(this.x * scalar, this.y * scalar);
+  };
+
+  Vector.prototype.clone = function VectorClone() {
+    return new Vector(this.x, this.y);
+  };
+
+  exports.Vector = Vector;
+}(Vector));
