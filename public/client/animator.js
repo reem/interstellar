73fac24adc6326e66658ec7d855cb88b7df76cc0
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-19 15:31:53 -0700

    Added real rendering.

--- public/client/animator.js
+++ public/client/animator.js
@@ -1,22 +1,24 @@
 var canvas = require('./index.js').canvas;
 
-var animateState = function (state) {
-  clear(canvas);
-  _.map(state.particles, renderParticle);
-  _.map(state.emitters, renderEmitter);
-  _.map(state.fields, renderField);
-};
-
 var renderParticle = function (particle) {
-  
+  canvas.context.fillStlye = particle.color;
+  canvas.context.fillRect(particle.pos.x, particle.pos.y, 1, 1);
 };
 
 var renderEmitter = function (emitter) {
 
 };
 
-var renderField = function (field) {
+var renderCircle = function (color, location, size) {
+  canvas.context.fillStyle = color;
+  canvas.context.beginPath();
+  canvas.context.arc(location.x, location.y, size, 0, Math.PI * 2);
+  canvas.context.closePath();
+  canvas.context.fill();
+};
 
+var renderField = function (field) {
+  renderCircle(field.color, field.pos, 4);
 };
 
 var renderFieldLine = function () {
@@ -24,12 +26,15 @@ var renderFieldLine = function () {
   throw new Error("Not implemented: renderFieldLine");
 };
 
-var renderCircle = function () {
-
-};
-
 var clear = function (canvas) {
   canvas.context.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+var animateState = function (state) {
+  clear(canvas);
+  _.map(state.particles, renderParticle);
+  _.map(state.emitters, renderEmitter);
+  _.map(state.fields, renderField);
 };
 
 exports.animator = animateState;
