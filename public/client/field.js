245608f4346a1e1529dd731c984a8de9db68a31f
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-19 14:42:48 -0700

    Ported last file to browserify.

--- public/client/field.js
+++ public/client/field.js
@@ -1,24 +1,20 @@
-var Field = {};
+var Field = function (pos, strength) {
+  this.pos = pos;
+  this.strength = strength;
+  this.color = this.strength > 0 ? "#f00" : "#0f0"; // Set green or red color.
+};
 
-(function (exports) {
-  var Field = function (pos, strength) {
-    this.pos = pos;
-    this.strength = strength;
-    this.color = this.strength > 0 ? "#f00" : "#0f0"; // Set green or red color.
-  };
+Field.prototype.force = function (xdiff, ydiff) {
+  return this.strength / Math.pow(xdiff * xdiff + ydiff * ydiff, 1.5);
+};
 
-  Field.prototype.force = function (xdiff, ydiff) {
-    return this.strength / Math.pow(xdiff * xdiff + ydiff * ydiff, 1.5);
-  };
+Field.prototype.affect = function (particle) {
+  var xDiff = this.pos.x - particle.pos.x;
+  var yDiff = this.pos.y - particle.pos.y; 
+  var force = this.force(xDiff, yDiff);
+  
+  // Don't create a vector obj.
+  particle.acceleration.fastAdd(xDiff * force, yDiff * force);
+};
 
-  Field.prototype.affect = function (particle) {
-    var xDiff = this.pos.x - particle.pos.x;
-    var yDiff = this.pos.y - particle.pos.y; 
-    var force = this.force(xDiff, yDiff);
-    
-    // Don't create a vector obj.
-    particle.acceleration.fastAdd(xDiff * force, yDiff * force);
-  };
-
-  exports.Field = Field;
-}(Field));
+exports.Field = Field;
