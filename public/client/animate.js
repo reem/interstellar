d995f1f84b224a9c0edd28710bc5cbeb74651a49
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-19 18:35:23 -0700

    Switched to pure js for a huge speedup.

--- public/client/animate.js
+++ public/client/animate.js
@@ -1,33 +1,14 @@
-var Lazy = require('./lazy.js').Lazy;
-console.dir(Lazy);
 var Utility = require('./utility.js');
 
-var innerAnimate = function (animation, sequence, wait) {
-  animation(sequence.first());
-  if (typeof wait === 'number') {
-    setTimeout(function () {
-      innerAnimate(animation, sequence.rest(), wait);
-    }, wait);
+var inner = function (animator, stepper, stopper, state) {
+  if (!stopper(state)) {
+    animator(state); // Blocking
+    window.requestAnimationFrame(function () { inner(animator, stepper, stopper, stepper(state)); });
   } else {
-    window.requestAnimationFrame(function () {
-      innerAnimate(animation, sequence.rest(), wait);
-    });
+    return state;
   }
 };
 
-var iterate = function (stepper, start) {
-  var current;
-
-  return Lazy.generate(function () {
-    if (current === undefined) {
-      current = start;
-    } else {
-      current = stepper(current);
-    }
-    return current;
-  });
-};
-
-exports.animate = function (animator, stepper, stopper, start, wait) {
-  innerAnimate(animator, iterate(stepper, start).takeWhile(Utility.not(stopper))); 
+exports.animate = function (animator, stepper, stopper, start) {
+  inner(animator, stepper, stopper, start); 
 };
