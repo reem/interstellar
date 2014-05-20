878c4567afed2359ed990cdb58208908c68f0770
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-19 18:53:20 -0700

    Improved animations.

--- public/client/animator.js
+++ public/client/animator.js
@@ -1,19 +1,19 @@
-exports.animator = function (canvas) {
+exports.animator = function (canvas, canvasContext) {
   var renderParticle = function (particle) {
-    canvas.context.fillStlye = particle.color;
-    canvas.context.fillRect(particle.pos.x, particle.pos.y, 1, 1);
+    canvasContext.fillStyle = particle.color;
+    canvasContext.fillRect(particle.pos.x, particle.pos.y, 1, 1);
   };
 
   var renderEmitter = function (emitter) {
-
+    renderCircle("#0ff", emitter.pos, 4);
   };
 
   var renderCircle = function (color, location, size) {
-    canvas.context.fillStyle = color;
-    canvas.context.beginPath();
-    canvas.context.arc(location.x, location.y, size, 0, Math.PI * 2);
-    canvas.context.closePath();
-    canvas.context.fill();
+    canvasContext.fillStyle = color;
+    canvasContext.beginPath();
+    canvasContext.arc(location.x, location.y, size, 0, Math.PI * 2);
+    canvasContext.closePath();
+    canvasContext.fill();
   };
 
   var renderField = function (field) {
@@ -25,16 +25,16 @@ exports.animator = function (canvas) {
     throw new Error("Not implemented: renderFieldLine");
   };
 
-  var clear = function (canvas) {
-    canvas.context.clearRect(0, 0, canvas.width, canvas.height);
+  var clear = function (canvasContext) {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
   };
 
   var animateState = function (state) {
-    clear(canvas);
+    clear(canvasContext);
     _.map(state.particles, renderParticle);
     _.map(state.emitters, renderEmitter);
     _.map(state.fields, renderField);
   };
 
-  exports.animator = animateState;
+  return animateState;
 };
