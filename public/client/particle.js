7f506e99e0df10ed236e1ddfbeb6f103c1579597
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-19 18:52:39 -0700

    Added coloring

--- public/client/particle.js
+++ public/client/particle.js
@@ -1,9 +1,13 @@
 var Vector = require('./vector.js').Vector;
 
-var Particle = function (pos, vel, acc) {
+var white = '#FFF';
+
+var Particle = function (pos, vel, acc, color) {
   this.pos = pos || new Vector(0, 0);
   this.vel = vel || new Vector(0, 0);
   this.acc = acc || new Vector(0, 0);
+  this.color = color || white;
+  _.bindAll(this, 'move');
 };
 
 Particle.prototype.move = function() {
