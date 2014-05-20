135ae0f54a12c82da1550749fc00ebd23e5a4031
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-19 18:56:13 -0700

    Implemented basic state and particle counting.

--- public/client/index.js
+++ public/client/index.js
@@ -5,24 +5,29 @@ var Update = require('./stepper.js');
 var Stopper = require('./stopper.js');
 var Emitter = require('./emitter.js');
 var Vector = require('./vector.js');
+var Field = require('./field.js');
 
 exports.xBound = window.innerWidth;
 exports.yBound = window.innerHeight;
+exports.particleLimit = 20000;
 
 var start = new State.State(
-  [], [], // particles, fields
+  [], 
+  [
+    // new Field.Field(new Vector.Vector(600, 300), -2000),
+    new Field.Field(new Vector.Vector(600, 400), 50000),
+  ], // particles, fields
   [new Emitter.Emitter(
-    new Vector.Vector(100, 100),
+    new Vector.Vector(500, 200),
     100,
-    5
+    20
   )]);
 
 exports.init = function () {
   var canvas = document.getElementById('main');
-  canvas.context = canvas.getContext('2d');
   canvas.width = exports.xBound;
   canvas.height = exports.yBound;
 
-  Animator.animator = require('./animator.js').animator(canvas);
+  Animator.animator = require('./animator.js').animator(canvas, canvas.getContext('2d'));
   Animate.animate(Animator.animator, Update.update, Stopper.stop, start);   
 };
