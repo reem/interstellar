0b84f8c9c6da30880c7061c8f4fb24e47ac383e6
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-19 18:34:00 -0700

    Made watch tasks useful.

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,8 +52,8 @@ module.exports = function(grunt) {
         ],
         tasks: [
           'jshint',
-          'browserify',
-          'uglify'
+          'browserify:dev',
+          'cssmin:minify'
         ]
       },
       options: {
