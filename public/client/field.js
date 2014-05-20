c1c5263fac51165a3fea9e2a12c35033cc242df3
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-19 18:55:57 -0700

    Added better field coloring and behavior.

--- public/client/field.js
+++ public/client/field.js
@@ -1,20 +1,18 @@
 var Field = function (pos, strength) {
   this.pos = pos;
   this.strength = strength;
-  this.color = this.strength > 0 ? "#f00" : "#0f0"; // Set green or red color.
-};
-
-Field.prototype.force = function (xdiff, ydiff) {
-  return this.strength / Math.pow(xdiff * xdiff + ydiff * ydiff, 1.5);
+  console.log(this.strength);
+  this.color = this.strength < 0 ? "#f00" : "#0f0"; // Set green or red color.
+  _.bindAll(this, 'affect');
 };
 
 Field.prototype.affect = function (particle) {
   var xDiff = this.pos.x - particle.pos.x;
   var yDiff = this.pos.y - particle.pos.y; 
-  var force = this.force(xDiff, yDiff);
-  
+  var force = this.strength / Math.pow(xDiff * xDiff + yDiff * yDiff, 1.5);
+
   // Don't create a vector obj.
-  particle.acceleration.fastAdd(xDiff * force, yDiff * force);
+  particle.acc.fastAdd(xDiff * force, yDiff * force);
 };
 
 exports.Field = Field;
