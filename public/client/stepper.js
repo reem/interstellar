27b16264cc917bac6537b1f57bf195bc7b63aae0
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-20 17:18:52 -0700

    Removed cd and debugged bounds checking.

--- public/client/stepper.js
+++ public/client/stepper.js
@@ -11,6 +11,11 @@ var update = function (state) {
     state.particles = state.particles.concat(_.flatten(Utility.methodMap(state.emitters, 'emit')));
   }
 
+  // Move all particles -- O(n)
+  for (i = 0; i < state.particles.length; i++) { // Making this a for loop sped up the code by 10x
+    state.particles[i].move();
+  }
+
   // Submit all particles to all fields -- O(n * m) -- the bottleneck
   var affecters = _.pluck(state.fields, 'affect'); // Cache property lookup.
   _.each(state.particles, function affectParticle (particle) {
@@ -20,36 +25,9 @@ var update = function (state) {
     }
   });
 
-  var collisionTree = new SplitTree(
-    new QuadTree.ParticleBox(
-      QuadTree.ParticlePoint.fromVector(new Vector.Vector(Index.xBound, Index.yBound)),
-      QuadTree.ParticlePoint.fromVector(new Vector.Vector(0, 0))
-    ));
-  for (i = 0; i < state.particles.length; i++) {
-    collisionTree.insert(new QuadTree.ParticlePoint(state.particles[i]));
-  }
-  for (i = 0; i < state.particles.length; i++) {
-    var near = collisionTree.near(new QuadTree.ParticlePoint(state.particles[i]));
-    if (!near) { continue; }
-    for (var j = 0; j < near.length; j++) {
-      if (Math.abs(state.particles[i].pos.x - near[j].particle.pos.x) <= 1 && Math.abs(state.particles[i].pos.y - near[j].particle.pos.y) <= 1) {
-        if (state.particles[i].vel.dotproduct(near[j].vel) > 0) {
-          var temp = state.particles[i].vel;
-          state.particles[i].vel = near[j].particle.vel;
-          near[j].particle.vel = temp;
-        }  
-      }
-    }
-  }
-
-  // Move all particles -- O(n)
-  for (i = 0; i < state.particles.length; i++) { // Making this a for loop sped up the code by 10x
-    state.particles[i].move();
-  }
-
   // Remove all particles that are out of bounds
   state.particles = _.reject(state.particles, function removeParticle (particle) {
-    return particle.pos.x > Index.xBound || particle.pos.y > Index.yBound;
+    return particle.pos.x > Index.xBound || particle.pos.y > Index.yBound || particle.pos.y < 0 || particle.pos.x < 0;
   }); 
   return state;
 };
