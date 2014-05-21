9a5a9fb65df61ba69dc893935293b20566f5c41d
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-20 17:16:04 -0700

    Added examples.

--- public/client/index.js
+++ public/client/index.js
@@ -9,19 +9,78 @@ var Field = require('./field.js');
 
 exports.xBound = window.innerWidth;
 exports.yBound = window.innerHeight;
-exports.particleLimit = 100000;
+exports.particleLimit = 1000000;
 
-var start = new State.State(
-  [], 
-  [
-    // new Field.Field(new Vector.Vector(600, 300), -2000),
-    new Field.Field(new Vector.Vector(600, 400), 25000),
-  ], // particles, fields
-  [new Emitter.Emitter(
-    new Vector.Vector(500, 200),
-    100,
-    10
-  )]);
+var examples = [
+  new State.State(
+    [], 
+    [
+      new Field.Field(new Vector.Vector(700, 400), -50000)
+    ],
+    [
+      new Emitter.Emitter(
+        new Vector.Vector(200, 400), 50, 50, undefined, 0, "white"
+      ),
+      new Emitter.Emitter(
+        new Vector.Vector(1200, 400), 50, 50, undefined, Math.PI, "white"
+      )
+    ]
+  ),
+  new State.State(
+    [], 
+    [
+      new Field.Field(new Vector.Vector(700, 400), 25000),
+      new Field.Field(new Vector.Vector(500, 400), -5000),
+      new Field.Field(new Vector.Vector(900, 400), -5000),
+      new Field.Field(new Vector.Vector(400, 400), 5000),
+      new Field.Field(new Vector.Vector(1000, 400), 5000),
+    ],
+    [
+      new Emitter.Emitter(
+        new Vector.Vector(400, 200), 25, 10, undefined, 0, "orange"
+      ),
+      new Emitter.Emitter(
+        new Vector.Vector(1000, 600), 25, 10, undefined, Math.PI, "yellow"
+      ),
+      new Emitter.Emitter(
+        new Vector.Vector(1000, 200), 25, 10, undefined, Math.PI, "pink"
+      ),
+      new Emitter.Emitter(
+        new Vector.Vector(400, 600), 25, 10, undefined, 0, "teal"
+      )
+    ]
+  ),
+  new State.State(
+    [], 
+    [
+      new Field.Field(new Vector.Vector(700, 400), 200),
+    ],
+    [
+      new Emitter.Emitter(
+        new Vector.Vector(650, 600), 50, 5, undefined, -Math.PI/2, "orange"
+      ),
+      new Emitter.Emitter(
+        new Vector.Vector(750, 600), 50, 5, undefined, -Math.PI/2, "orange"
+      )
+    ]
+  ),
+  new State.State(
+    [], 
+    [
+      new Field.Field(new Vector.Vector(500, 400), -20000),
+      new Field.Field(new Vector.Vector(900, 400), -20000),
+      new Field.Field(new Vector.Vector(700, 600), -20000),
+      new Field.Field(new Vector.Vector(700, 200), -20000),
+    ],
+    [
+      new Emitter.Emitter(
+        new Vector.Vector(700, 400), 400, 20, Math.PI * 2, Math.PI * 2, "orange"
+      )
+    ]
+  ),
+]; 
+
+var start = examples[0]; 
 
 exports.init = function () {
   var canvas = document.getElementById('main');
