4016695de9d8a16db929979ef61d9339904b2b6e
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-20 17:00:21 -0700

    Allowed emitters to control the color of the particles they emit.

--- public/client/emitter.js
+++ public/client/emitter.js
@@ -1,12 +1,13 @@
 var Vector = require('./vector.js').Vector;
 var Particle = require('./particle.js').Particle;
 
-var Emitter = function (pos, ppf, startVel, spread, color) {
+var Emitter = function (pos, ppf, startVel, spread, direction, color) {
   this.pos = pos || new Vector(0, 0);
   this.ppf = ppf || 10;
   this.startVel = startVel === undefined ? 0 : startVel;
-  this.spread = spread || Math.PI / 64;
+  this.spread = spread === undefined ? Math.PI / 64 : spread;
   this.drawColor = color || '#999';
+  this.direction = direction || 0;
   _.bindAll(this, 'emit');
 };
 
@@ -15,8 +16,9 @@ Emitter.prototype.emit = function() {
   return _.times(this.ppf, function () {
     return new Particle(
       this.pos.clone(),
-      Vector.fromAngle(this.startVel * (Math.random() + 0.2), this.spread - (Math.random() * this.spread * 2)) 
-      // default acceleration to Vec(0, 0)
+      Vector.fromAngle(this.startVel * (Math.random() + 0.2), this.spread - (Math.random() * this.spread * 2) + this.direction), 
+      undefined, // default acceleration to Vec(0, 0)
+      this.drawColor
     );
   }.bind(this));
 };
